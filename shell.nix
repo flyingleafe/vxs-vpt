b6bdfd939eea2222ba115fcc4700faebfda87eea
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2020-02-29 00:33:57 +0000

    Run jupyterlab from virtualenv on Linux

--- shell.nix
+++ shell.nix
@@ -4,18 +4,23 @@ with pkgs;
 
 let
   virtualenvDir = "pythonenv";
+  manylinuxLibPath = stdenv.lib.makeLibraryPath [(callPackage ./manylinux1.nix {}).package];
 in
 mkShell {
   buildInputs = [
+    nodejs
     python37Packages.virtualenv
   ];
 
   # Fix wheel building and init virtualenv
   shellHook = ''
+    unset SOURCE_DATE_EPOCH
     if [ ! -d "${virtualenvDir}" ]; then
       python -m venv ${virtualenvDir}
     fi
+    echo "manylinux1_compatible = True" > ${virtualenvDir}/lib/python3.7/_manylinux.py
     source ${virtualenvDir}/bin/activate
+    export LD_LIBRARY_PATH=${manylinuxLibPath}
     export TMPDIR=/tmp
   '';
 }
